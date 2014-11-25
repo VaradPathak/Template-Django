9a7335d03ef7ee3e073f7e35396eb55690be74f3
Varad Pathak <varad.pathak@gifts.com>
2014-11-24 23:57:17 -0800

    Adding support for Tastypie

---
+++ Template_Django/api.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from tastypie import fields
+from tastypie.resources import ModelResource
+
+
+class UserResource(ModelResource):
+
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
